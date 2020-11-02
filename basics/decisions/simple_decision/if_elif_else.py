8f4de27723890d5e89b379eabc9b0d9cde149efc
repl.it user <replituser@example.com>
2020-11-02 13:55:05 +0000

    broken

--- basics/decisions/simple_decision/if_elif_else.py
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -1,12 +1,15 @@
 #using elif to provide options and display a suitable response
-print("Towards which direction should I paint(up, down, left or right?"))
-direction = input()
+print("Towards which direction should I paint up, down, left or right?")
 
+direction = input()
 if (direction == "up", "Up"):
   print("I am painting in the upward direction!")
+
 elif (direction == "down", "Down"):
   print("I am painting in the downward direction!")
 elif (direction == "left", "Left"):
   print("I am painting in the left direction!")
 elif (direction == "right", "Right"):
   print("I am painting in the right direction!")
+else:
+  print("I don't know what you mean")
