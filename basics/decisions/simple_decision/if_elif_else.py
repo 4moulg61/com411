b3fa3ba9eb9cc809188ff6b263a449bf0cf12f7d
repl.it user <replituser@example.com>
2020-11-06 14:03:36 +0000

    using the modulo operator

--- basics/decisions/simple_decision/if_elif_else.py
+++ basics/decisions/simple_decision/if_elif_else.py
@@ -1,12 +1,14 @@
-#using elif to provide options and display a suitable response
-print("Towards which direction should I paint(up, down, left or right?"))
-direction = input()
+#using elif to provide options and display a suitable response by asking the user to input a direction and displaying the relevant response
+print("Towards which direction should I paint (up, down, left or right)?")
+direction = input() 
 
-if (direction == "up", "Up"):
-  print("I am painting in the upward direction!")
-elif (direction == "down", "Down"):
-  print("I am painting in the downward direction!")
-elif (direction == "left", "Left"):
-  print("I am painting in the left direction!")
-elif (direction == "right", "Right"):
-  print("I am painting in the right direction!")
+if (direction == "up"):
+    print("\nI am painting in the upward direction!")
+elif (direction == "down"):
+    print("\nI am painting in the downward direction!")
+elif (direction == "left"):
+    print("\nI am painting in the leftward direction!")
+elif (direction == "right"):
+    print("\nI am painting in the rightward direction!")
+else:
+    print("\nI don't know what to do")
---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,12 @@
+#asking user to input a number
+print("Please enter a whole number")
+number = input()
+
+
+
+
+
+if (number % 2 == 0):
+    print("\nThe number {} is an even number.".format(number))
+else:
+    print("\nThe number {} is an odd number.".format(number))
