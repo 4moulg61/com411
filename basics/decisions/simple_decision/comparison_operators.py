4edd0276685b6cf4ff2b692fe04a1f67f7044d11
repl.it user <replituser@example.com>
2020-11-12 01:17:01 +0000

    added code to display an ascii robot with dynamic eyes

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,4 @@
+#asking the user to input a number
+print("Please enter the first number:")
+number_one = int(input())
+print("")
---
+++ basics/input/ascii_robot.py
@@ -0,0 +1,11 @@
+# displaying a robot
+#asking user for input for eye
+print("Please enter a character for the eye:")
+eye = input()
+print("Beep's expression is as follows:")
+print("    _______ ")
+print("   / _____ \    ")
+print("  | |" + eye + "___" + eye + "| | ")
+print("   \_______/ ")
+print("       |    ")
+print("      /\ ")
